35799e084851521e6132d9edfe5f657cceb10146
Dennis Bettels <dbettels1@gmail.com>
2018-03-12 13:32:19 +0000

    refac rename var file for modifying

--- Code/refac_rename_var.erl
+++ Code/refac_rename_var.erl
@@ -1,3 +1,51 @@
+%% Copyright (c) 2010, Huiqing Li, Simon Thompson  
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are met:
+%%     %% Redistributions of source code must retain the above copyright
+%%       notice, this list of conditions and the following disclaimer.
+%%     %% Redistributions in binary form must reproduce the above copyright
+%%       notice, this list of conditions and the following disclaimer in the
+%%       documentation and/or other materials provided with the distribution.
+%%     %% Neither the name of the copyright holders nor the
+%%       names of its contributors may be used to endorse or promote products
+%%       derived from this software without specific prior written permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''
+%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+%% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+%% BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+%% BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+%% WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+%% OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+%% ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%% =====================================================================
+%% Refactoring: Rename a variable name.
+%%
+%% Author contact: hl@kent.ac.uk, sjt@kent.ac.uk
+%%
+%% =====================================================================
+%%
+%% @doc Rename a variable with a user-supplied new variable name.
+%% <p> To apply this refactoring, point the cursor to the beginning of any occurrence of this variable, then select
+%% <em> Rename Variable Name </em> from the <em> Refactor </em> menu, after that the refactorer will prompt to enter
+%% the new parameter name in the mini-buffer.
+%% </p>
+%% <p> This refactoring has a local effect, i.e., it only affects the function in which the refactoring is initialised.
+%% </p>
+%% <p> The following <em> side-conditions </em> apply to this refactoring:
+%% <li> The new variable name should not conflict with any of the declared variable names in the same scope;</li>
+%% <li> The new name should not shadow any of the existing variables in the outer scopes, or be shadowed by any of
+%% of existing variables in the inner scopes, i.e., renaming to the new name should not change the semantics of the
+%% program.</li>
+%% </p>
+%% @end
+
+%% @private 
 -module(refac_rename_var).
 
 -export([rename_var/6, rename_var/7, rename_var_composite/7, rename_var_eclipse/6]).
