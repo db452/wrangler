35da46fb1a883f39ec6d0eadb885e97c00299675
db452 <db452@kent.ac.uk>
2018-03-19 11:51:17 +0000

    Got Codefile to communicate with Erlang node

--- Code/Codefile.java
+++ Code/Codefile.java
@@ -10,20 +10,75 @@ import java.io.IOException;
 import com.ericsson.otp.erlang.*;
 public class Codefile {
 
-	public static void main(String[] _args) throws IOException, OtpErlangExit, OtpErlangDecodeException {
+	private static OtpSelf client;
+	private static OtpPeer server;
+	private static OtpConnection connection;
+
+	public static void main(String[] args) throws IOException, OtpErlangExit, OtpErlangDecodeException, OtpAuthException {
 		
-			OtpNode node = new OtpNode("MainNode");
-			OtpMbox mbox = node.createMbox("server");  
+		
 
-			OtpErlangObject o;
- 			OtpErlangTuple msg;
-			OtpErlangPid from;			 
- 			OtpErlangAtom ok = new OtpErlangAtom("ok");
+		
+		client = new OtpSelf("client", "cookie");
+		server = new OtpPeer("server@db-VirtualBox");
+		connection = client.connect(server);
+		
+		invokeWrangler();
+		renameFunction();
+		stopWrangler();
+	}
+	
+	private static void invokeWrangler() throws IOException,OtpAuthException, OtpErlangExit, OtpErlangDecodeException {
 
- 			mbox.send(from,tuple);
+		connection.sendRPC("api_wrangler", "start", startArgs());
+		OtpErlangObject response = connection.receiveMsg().getMsg();
+		System.out.println(response);
 
-			//communicate with rename_var by including them in the same package
-			//receive input from html file.
+
 
 	}
+
+	private static void renameFunction() throws IOException,OtpAuthException, OtpErlangExit, OtpErlangDecodeException{
+
+		String fileLoc = "/home/db/Desktop/test.erl";
+		String oldVar = "blobLoop";
+		int arity = 0;
+		String newVar = "fLoop";
+		String emacs = "/usr/bin/emacs"; //Please replace with emacs (or eclipse) location!
+		
+		connection.sendRPC("api_wrangler", "rename_fun", renamefunArgs(fileLoc, oldVar, arity, newVar, emacs));
+		OtpErlangObject response1 = connection.receiveMsg().getMsg();
+		System.out.println(response1);
+
+	}
+
+	private static void stopWrangler() throws IOException,OtpAuthException, OtpErlangExit, OtpErlangDecodeException{
+
+		connection.sendRPC("api_wrangler", "stop", startArgs());
+		OtpErlangObject response = connection.receiveMsg().getMsg();
+		System.out.println(response);
+
+	}
+	
+	
+	private static OtpErlangObject[] startArgs() {
+		return new OtpErlangObject[] { 
+
+		};
+	}
+
+	private static OtpErlangObject[] renamefunArgs(String fileLoc, String oldVar, int arity, String newVar, String emacs){
+
+		return new OtpErlangObject[] { 
+			new OtpErlangString(fileLoc),
+			new OtpErlangAtom(oldVar),
+			new OtpErlangInt(arity),
+			new OtpErlangAtom(newVar),
+			new OtpErlangList(emacs)
+		};
+
+	} 
+
+
 }
+
